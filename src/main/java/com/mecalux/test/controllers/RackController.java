afbe10566b014dec529bb58a87d317d6237e8346
Luis Jimenez <luisjjn@ext.inditex.com>
2025-05-13 02:38:20 +0200

    added logs

--- src/main/java/com/mecalux/test/controllers/RackController.java
+++ src/main/java/com/mecalux/test/controllers/RackController.java
@@ -4,6 +4,7 @@ import com.mecalux.test.domain.requests.RackRequest;
 import com.mecalux.test.services.RackService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,26 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rack")
 @RequiredArgsConstructor
+@Slf4j
 public class RackController {
+  private static final String LOG_PREFIX = "[RACK REST CONTROLLER]";
+
   private final RackService rackService;
 
   @PostMapping
   public ResponseEntity<?> add(@RequestBody @Valid RackRequest request) {
-      return ResponseEntity.ok(this.rackService.add(request));
+    log.debug("{} Adding rack with request: {}", LOG_PREFIX, request);
+    return ResponseEntity.ok(this.rackService.add(request));
   }
 
   @GetMapping("/all")
   public ResponseEntity<?> getAll() {
+    log.debug("{} Getting racks", LOG_PREFIX);
     return ResponseEntity.ok(this.rackService.getAll());
   }
 
   @GetMapping("/warehouse/{warehouseId}")
   public ResponseEntity<?> getByWarehouseId(@PathVariable Integer warehouseId) {
+    log.debug("{} Getting racks by warehouse id: {}", LOG_PREFIX, warehouseId);
     return ResponseEntity.ok(this.rackService.getByWarehouseId(warehouseId));
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<?> getById(@PathVariable Integer id) {
+    log.debug("{} Getting rack with id: {}", LOG_PREFIX, id);
     return ResponseEntity.ok(this.rackService.getById(id));
   }
 }
--- src/main/java/com/mecalux/test/controllers/WarehouseController.java
+++ src/main/java/com/mecalux/test/controllers/WarehouseController.java
@@ -4,6 +4,7 @@ import com.mecalux.test.domain.requests.WarehouseRequest;
 import com.mecalux.test.services.WarehouseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,21 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/warehouse")
 @RequiredArgsConstructor
+@Slf4j
 public class WarehouseController {
+  private static final String LOG_PREFIX = "[WAREHOUSE REST CONTROLLER]";
+
   private final WarehouseService warehouseService;
 
   @PostMapping
   public ResponseEntity<?> add(@RequestBody @Valid WarehouseRequest request) {
+    log.debug("{} Adding warehouse with request: {}", LOG_PREFIX, request);
     return ResponseEntity.ok(this.warehouseService.add(request));
   }
 
   @GetMapping("/all")
   public ResponseEntity<?> getAll() {
+    log.debug("{} Getting warehouses", LOG_PREFIX);
     return ResponseEntity.ok(this.warehouseService.getAll());
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<?> getById(@PathVariable Integer id) {
+    log.debug("{} Getting warehouse with id: {}", LOG_PREFIX, id);
     return ResponseEntity.ok(this.warehouseService.getById(id));
   }
 }
